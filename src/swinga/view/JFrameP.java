9e9c07fa58a3252c4430ca79328dcbabcdbb861f
Administrateur <Administrateur@172.26.10.92>
2017-05-10 12:42:24 +0200

    v ok 0

---
+++ src/swinga/view/JFrameP.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swinga.view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class JFrameP extends JFrame {
+
+    public JFrameP() {
+
+        this.setSize(600, 600);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//arrete le processus à la fermeture de la fenetre
+
+        JPanel panneau = new JPanel();
+
+//        for (int i = 0; i < 100; i++) {
+////            panneau.add(new JButton("" +( i + 1)));
+//            panneau.add(new JButton("" + (i + 1)));
+//        }
+//        panneau.setLayout(new GridLayout(10, 10));
+//        JButton bouton = new JButton("etki");
+//        panneau.add(bouton);
+        this.add(panneau);
+//        this.add(new JPanelHotel()); 
+        this.add(new JPanelPrincipale());
+        this.setVisible(true);
+
+    }
+
+}
